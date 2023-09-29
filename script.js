6121a8f44b3835743ef52392eb43764ff75e29e4
Adi6583 <92431471+Adi6583@users.noreply.github.com>
2023-09-29 10:52:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const locationInput = document.getElementById('locationInput');
+    const searchButton = document.getElementById('searchButton');
+    const unitSelect = document.getElementById('unitSelect');
+    const weatherData = document.getElementById('weatherData');
+
+    searchButton.addEventListener('click', () => {
+        const location = locationInput.value;
+        const unit = unitSelect.value;
+
+        if (location.trim() === '') {
+            alert('Please enter a location');
+            return;
+        }
+
+        //  API key
+        const apiKey = 'aa427572b4e45c1f1eb239a4c58fa59f';
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unit}&appid=${apiKey}`;
+
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network not found');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const cityName = data.name;
+                const temperature = data.main.temp;
+                const humidity = data.main.humidity;
+                const windSpeed = data.wind.speed;
+                const weatherDescription = data.weather[0].description;
+
+                // Check temperature and set background color
+                if (temperature >= 30) {
+                    document.body.style.backgroundColor = 'red';
+                } else {
+                    document.body.style.backgroundColor = 'lightgreen';
+                }
+                
+                weatherData.innerHTML = `
+                    <h1>Weather in ${cityName}</h1>
+                    <p>Temperature: ${temperature}°${unit === 'metric' ? 'C' : 'F'}</p>
+                    <p>Humidity: ${humidity}%</p>
+                    <p>Wind Speed: ${windSpeed} m/s</p>
+                    <p>Conditions: ${weatherDescription}</p>
+                `;
+            })
+            .catch(error => {
+                console.error('There was a problem fetching the weather data:', error);
+                weatherData.innerHTML = 'Sorry, there was an error fetching the weather data.';
+            });
+    });
+});
